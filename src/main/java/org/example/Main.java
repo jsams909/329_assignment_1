4235bf4c794dc32f6f25d6203e528184d54ebda0
josh samson <samsonknight909@gmail.com>
2025-02-04 19:56:50 -0500

    Created a node and PQList class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,52 +1,8 @@
 package org.example;
 import java.util.*;
 
-     class Node{
-        int data;
-        Node next;
-        Node head;
-        Node tail;
-        public Node(int data) {
-            this.data = (int) data;
-            this.next = head.next;
-            head.next = tail;
-
-}
-
-class LinkedList implements PriorityQueueInterface{
-    @Override
-    public void add(Object o) {
-    Node backNode = new Node(data);
-    tail.next = backNode;
-    tail = backNode;
 
 
-    }
-
-    @Override
-    public Player getHighScore() {
-        return null;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public int getSize() {
-        return 0;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-
-    }
-
-    }
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
---
+++ src/main/java/org/example/PQList.java
@@ -0,0 +1,50 @@
+package org.example;
+
+
+
+class Node{
+    int data;
+    Node next;
+    Node head;
+    Node tail;
+    public Node(int data) {
+        this.data = (int) data;
+        this.next = head.next;
+        head.next = tail;
+
+    }
+
+public class PQList implements PriorityQueueInterface{
+    @Override
+    public void add(Object o) {
+        Node backNode = new Node(data);
+        tail.next = backNode;
+        tail = backNode;
+
+
+    }
+
+    @Override
+    public Player getHighScore() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+
+}}
+
+
