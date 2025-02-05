79a4aef50fa9d23c021402a84246fc05211b5f41
josh samson <samsonknight909@gmail.com>
2025-02-04 19:46:14 -0500

    Moved player to its own class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,12 @@
 package org.example;
 import java.util.*;
 
-class Player{
-    String name;
-    int score;
-    Player() {
-    }
-
-    public Player(String name, int score) {
-
-    }
-    Player(Player other){
-        this.name=other.name;
-        this.score=other.score;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getScore() {
-        return score;
-    }
-    public void setScore(int score) {
-        this.score = score;
-    }
-    Player createClone(){
-        return new Player(this);
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Player player = (Player) o;
-        return name.equals(player.name);
-    }
-    private class Node<T>{
+     class Node{
         int data;
         Node next;
         Node head;
         Node tail;
-        public Node(T data) {
+        public Node(int data) {
             this.data = (int) data;
             this.next = head.next;
             head.next = tail;
@@ -88,7 +53,7 @@ class LinkedList implements PriorityQueueInterface{
     }
 
     }
-}
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
