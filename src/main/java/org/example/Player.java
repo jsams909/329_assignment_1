40e1e08666902a1bdbe615f47779e6e208429baf
josh samson <samsonknight909@gmail.com>
2025-02-09 09:22:31 -0500

    Edited my player class

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,43 @@
+package org.example;
+
+class Player{
+    String name;
+    int score;
+
+    Player() {
+    }
+
+    Player(String name, int score, Player p) {
+        this.name=name;
+        this.score=score;
+    }
+
+    Player(Player other){
+        this.name=other.name;
+        this.score=other.score;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getScore() {
+        return score;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    Player createClone(String name, int score) {
+        return new Player(this);
+    }
+        @Override
+        public boolean equals(Object play) {
+            if (this == play) return true;
+            if (play == null || getClass() != play.getClass()) return false;
+            Player player = (Player) play;
+            return name.equals(player.name);
+        }
+    }
